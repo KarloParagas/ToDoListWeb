58d3b6d3fab22e1be5c5d0ebea59f37e24eda7d9
KarloParagas <karloparagas@gmail.com>
2019-10-22 14:47:28 -0700

    Adding and displaying todo item is functional

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,51 @@
  */
 var ToDoItem = /** @class */ (function () {
     function ToDoItem(task) {
-        if (task == null) {
-            throw ;
-        }
         this.title = task;
     }
     return ToDoItem;
 }());
+var myItem = new ToDoItem("Finish class");
+window.onload = function () {
+    var addBtn = document.querySelector("form > input[type=button]");
+    addBtn.onclick = main;
+};
+function main() {
+    //Get ToDoItem from user
+    var item = getItem();
+    //Display new ToDoItem on page
+    displayToDoItem(item);
+    //Save ToDoItem
+}
+/**
+ * Move selected task to completed section of the webpage
+ */
+function markAsComplete() {
+    var currItem = this;
+    var completedItems = document.getElementById("completed");
+    completedItems.appendChild(currItem);
+}
+/**
+ * Displays ToDoItem on the page
+ * @param iten
+ */
+function displayToDoItem(item) {
+    var div = document.createElement("div"); //createElement creates whatever element you pass in it
+    div.onclick = markAsComplete;
+    div.innerHTML = '<input type="checkbox">' + item.title;
+    //Display new div on page
+    var displayDiv = document.getElementById("todo");
+    displayDiv.appendChild(div);
+}
+/**
+ * Gets the user input todo item from the form
+ */
+function getItem() {
+    //Get all input from form and return ToDoItem
+    var title = document.getElementById("title").value;
+    var item = new ToDoItem(title);
+    var deadline = document.getElementById("deadline").value;
+    item.deadline = new Date(deadline);
+    item.isCompleted = false;
+    return item;
+}
--- typescripts/main.ts
+++ typescripts/main.ts
@@ -8,9 +8,64 @@ class ToDoItem{
     isCompleted:boolean;
 
     constructor(task:string){
-        if(task == null){
-            throw;
-        }
         this.title = task;
-    }
+    }   
+}
+
+let myItem = new ToDoItem("Finish class");
+
+window.onload = function(){
+    let addBtn = <HTMLElement>document.querySelector("form > input[type=button]");
+    addBtn.onclick = main;
+};
+
+function main(){
+    //Get ToDoItem from user
+    let item:ToDoItem = getItem();    
+
+    //Display new ToDoItem on page
+    displayToDoItem(item);
+
+    //Save ToDoItem
+}
+
+/**
+ * Move selected task to completed section of the webpage
+ */
+function markAsComplete(){
+    let currItem = <HTMLDivElement>this;
+
+    let completedItems = document.getElementById("completed");
+
+    completedItems.appendChild(currItem);
+}
+
+/**
+ * Displays ToDoItem on the page
+ * @param iten 
+ */
+function displayToDoItem(item:ToDoItem):void{
+    let div = document.createElement("div"); //createElement creates whatever element you pass in it
+    div.onclick = markAsComplete;
+
+    div.innerHTML = '<input type="checkbox">' + item.title;
+
+    //Display new div on page
+    let displayDiv = document.getElementById("todo");
+    displayDiv.appendChild(div);
+}
+
+/**
+ * Gets the user input todo item from the form
+ */
+function getItem():ToDoItem{
+    //Get all input from form and return ToDoItem
+    let title = (<HTMLInputElement>document.getElementById("title")).value;
+    let item = new ToDoItem(title);
+
+    let deadline = (<HTMLInputElement>document.getElementById("deadline")).value;
+    item.deadline = new Date(deadline);
+    item.isCompleted = false;
+
+    return item;
 }
